fd71cf3c389b26b8f7677fc7096120b79b99d86c
ishita <ishita.email@example.com>
2024-06-10 00:17:10 -0400

    success

---
+++ my-ecommerce-app/src/app/data/cardSlide.js
@@ -0,0 +1,98 @@
+export const cardSilderData = [
+    {
+      title: "Featured Products",
+      items: [
+        {
+          id: 1,
+          title: "Shirt 1",
+          image: "/shirt.webp",
+          description: "This is card 1",
+          price: "100",
+        },
+        {
+            id: 2,
+            title: "jacket",
+            image: "/jacket.webp",
+            description: "This is card 2",
+            price: "100",
+        },
+        {
+            id: 3,
+            title: "jean",
+            image: "/jeans.webp",
+            description: "This is card 3",
+            price: "200",
+        },
+        {
+            id: 4,
+            title: "Cloth",
+            image: "/shirt.webp",
+            description: "This is card 4",
+            price: "200",
+        },
+        {
+            id: 5,
+            title: "jean",
+            image: "/jeans.webp",
+            description: "This is card 5",
+            price: "300",
+          
+        },
+        {
+            id: 6,
+            title: "Cloth ",
+            image: "/shirt.webp",
+            description: "This is card 6",
+            price: "300",
+        },
+          
+      ],
+    },
+    {
+      title: "Standard Edition",
+      items: [
+        {
+          id: 1,
+          title: "Cloth",
+          image: "/shirt.webp",
+          description: "This is book 1",
+          price: "100",
+        },
+        {
+          id: 2,
+          title: "Cloth",
+          image: "/shirt.webp",
+          description: "This is book 2",
+          price: "200",
+        },
+        {
+          id: 3,
+          title: "Cloth 3",
+          image: "/shirt.webp",
+          description: "This is book 3",
+          price: "300",
+        },
+        {
+          id: 4,
+          title: "Cloth 4",
+          image: "/shirt.webp",
+          description: "This is book 4",
+          price: "400",
+        },
+        {
+          id: 5,
+          title: "Cloth 5",
+          image: "/shirt.webp",
+          description: "This is book 5",
+          price: "500",
+        },
+        {
+          id: 6,
+          title: "Cloth 6",
+          image: "/shirt.webp",
+          description: "This is book 6",
+          price: "600",
+        },
+      ],
+    },
+  ];
